d89faf2ae9d38d986e0b889e85c22a604271f7a6
Maximilian Staats <maximilian.staats@marqably.com>
2024-11-09 23:29:26 +0100

    basic rest service

--- apps/backend/src/app/recommendations/recommendations.controller.ts
+++ apps/backend/src/app/recommendations/recommendations.controller.ts
@@ -1,13 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { RecommendationsService } from './recommendations.service';
-
 @Controller('recommendations')
 export class RecommendationsController {
   constructor(
     private readonly recommendationsService: RecommendationsService
   ) {}
   @Get()
-  public async getRecommendations(): Promise<unknown> {
-    return this.recommendationsService.getRecommendations();
+  public async getRecommendations(): Promise<string> {
+    const recommendations =
+      await this.recommendationsService.getRecommendationsByString('hello');
+    return JSON.stringify(recommendations);
   }
 }
--- apps/backend/src/app/recommendations/recommendations.service.ts
+++ apps/backend/src/app/recommendations/recommendations.service.ts
@@ -1,8 +1,55 @@
 import { Injectable } from '@nestjs/common';
+import { ChromaClient } from 'chromadb';
 
 @Injectable()
 export class RecommendationsService {
-  public async getRecommendations(): Promise<unknown> {
-    return [];
+  private readonly client: ChromaClient;
+  private collection;
+
+  constructor() {
+    console.log('starting RecommendationsService');
+
+    this.client = new ChromaClient();
+    this.init()
+      .then(() => {
+        console.log('RecommendationsService initialized');
+      })
+      .catch((err) => {
+        console.error('RecommendationsService failed to initialize', err);
+      });
   }
+
+  private async init() {
+    this.collection = await this.client.getOrCreateCollection({
+      name: 'recommendations',
+    });
+    this.collection.add({
+      ids: ['1', '2'],
+      documents: ['This text says hello', 'this text says goodbye'],
+    });
+  }
+
+  public async addEntries(entries: Entry[]) {
+    return await this.collection.add({
+      ids: entries.map((entry) => entry.id),
+      documents: entries.map((entry) => entry.document),
+    });
+  }
+
+  public async getRecommendationsByString(
+    query: string,
+    nResults = 2
+  ): Promise<string[]> {
+    const results = await this.collection.query({
+      queryTexts: query, // Chroma will embed this for you
+      nResults, // how many results to return
+    });
+    console.log('results', results.documents);
+    return results.documents;
+  }
+}
+
+interface Entry {
+  id: string;
+  document: string;
 }
