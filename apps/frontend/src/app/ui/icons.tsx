6c4c718348375585bbe8b34caa23af1e0b73383d
Lukas Bachlechner <lukas.bachlechner@marqably.com>
2024-11-10 10:41:52 +0100

    chore: add icons

---
+++ apps/frontend/src/app/ui/icons.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+export const HeartIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      x-bind:width="size"
+      x-bind:height="size"
+      viewBox="0 0 24 24"
+      fill="none"
+      width="24"
+      height="24"
+      stroke-width="2"
+      stroke-linejoin="round"
+      stroke-linecap="round"
+      x-bind:stroke-width="stroke"
+      stroke="currentColor"
+    >
+      <path d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572"></path>
+    </svg>
+  );
+};
+
+export const CloseIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      x-bind:width="size"
+      x-bind:height="size"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      x-bind:stroke-width="stroke"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      width="24"
+      height="24"
+      stroke-width="2"
+    >
+      <path d="M18 6l-12 12"></path>
+      <path d="M6 6l12 12"></path>
+    </svg>
+  );
+};
